d62837b014e616c99629facad1ab2922ca9454f1
Den Pol <pravosleva@uremont.com>
2019-10-16 18:16:57 +0300

    Updates

--- 7-styling-components/components/MyLayout.js
+++ 7-styling-components/components/MyLayout.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import Header from './Header'
 
 const layoutStyle = {
@@ -9,6 +11,14 @@ const layoutStyle = {
 export default function Layout(props) {
   return (
     <div style={layoutStyle}>
+      <Head>
+        <title>Default title</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+      </Head>
       <Header />
       {props.children}
     </div>
