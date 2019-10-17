a0d833cbacfd7d4e3b1d7b11dea140f5aa8a6f9d
Den Pol <pravosleva@uremont.com>
2019-10-17 13:55:03 +0300

    Lazy loading modules

--- E2-lazy-loading-modules/lib/load-db.js
+++ E2-lazy-loading-modules/lib/load-db.js
@@ -1,18 +1,19 @@
 export default async function loadDb() {
-  const firebase = require('firebase/app')
-  require('firebase/database')
+  const firebase = await import('firebase/app');
+  await import('firebase/database');
+  // Firebase will be loaded from static/chunks/[rand-str].js once.
 
   try {
     firebase.initializeApp({
       databaseURL: 'https://hacker-news.firebaseio.com'
-    })
+    });
   } catch (err) {
     // we skip the "already exists" message which is
     // not an actual error when we're hot-reloading
     if (!/already exists/.test(err.message)) {
-      console.error('Firebase initialization error', err.stack)
+      console.error('Firebase initialization error', err.stack);
     }
   }
 
-  return firebase.database().ref('v0')
+  return firebase.database().ref('v0');
 }
