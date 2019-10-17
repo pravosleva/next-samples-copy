3e5aa344b156b22eabbf327df92ed4f140b484a2
Den Pol <pravosleva@uremont.com>
2019-10-17 13:56:09 +0300

    Lazy loading modules

--- E2-lazy-loading-modules/lib/load-db.js
+++ E2-lazy-loading-modules/lib/load-db.js
@@ -2,6 +2,18 @@ export default async function loadDb() {
   const firebase = await import('firebase/app');
   await import('firebase/database');
   // Firebase will be loaded from static/chunks/[rand-str].js once.
+  /*
+    As you have witnessed, it only loads when you navigate a page for the first
+    time. Here is what is actually happening.
+
+    At the first time, the getInitialProps in the page pages/p/[id].js imports
+    the firebase/app and firebase/database modules (via lib/load-db.js). So,
+    the app loads the bundle.
+
+    The second time, the page pages/index.js imports the firebase/app and
+    firebase/database modules. But at that time, they are already loaded and
+    there is no reason to load them again.
+  */
 
   try {
     firebase.initializeApp({
